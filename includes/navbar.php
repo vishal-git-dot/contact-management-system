630a55448d503750c59c2cd8ad01d7b22acbfbf6
Vishal Purushothaman <vega1four@gmail.com>
2026-05-09 22:47:32 +0530

    Add navbar component to includes directory

---
+++ includes/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+
+    <div class="logo">
+        ContactManager
+    </div>
+
+    <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="add_contact.php">Add Contact</a></li>
+    </ul>
+
+</nav>
